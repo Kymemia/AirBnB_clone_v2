58c9ab1f9dd035c35709ac852c5ac7ab1b0aaafd
Kymemia <kimemiachristopherr@gmail.com>
2024-05-19 20:26:09 +0300

    number template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""This is a script that starts a Flask web application"""
+
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """property that returns statement on success"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """property that returns statement on success"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """property that returns C followed
+    by the value of the text variable"""
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route("/python/", defaults={'text': 'is_cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text):
+    """property that returns Python,
+    followed by the value of the text variable"""
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """property that displays 'n is a number'
+    only if n is an integer"""
+    return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """property that displays a HTML page
+    only if n is a integer"""
+    le_number = f"number: {n}"
+    return render_template('5-number.html', my_object=le_number)
+
+
+if __name__ == "__main__":
+    """ensure the application is running
+    on 0.0.0.0, port 5000
+    """
+    app.run(host="0.0.0.0", port=5000, debug=True)
